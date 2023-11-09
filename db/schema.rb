b7b2a157dc91531173a6d0ed71cf5e61183f31b5
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-09 13:29:07 +0000

    お酒の組み合わせ系のサイト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_09_131755) do
+ActiveRecord::Schema.define(version: 2023_11_09_132305) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "liquors", force: :cascade do |t|
     t.string "name"
