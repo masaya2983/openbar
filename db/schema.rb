7e2f47a1d4c89555b7b747566e4bb33346500411
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-10 11:01:01 +0000

    お酒の組み合わせ系のサイト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_09_133533) do
+ActiveRecord::Schema.define(version: 2023_11_10_105311) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
@@ -19,6 +31,14 @@ ActiveRecord::Schema.define(version: 2023_11_09_133533) do
   end
 
   create_table "liquors", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "places", force: :cascade do |t|
     t.string "name"
     t.integer "category_id"
     t.integer "user_id"
