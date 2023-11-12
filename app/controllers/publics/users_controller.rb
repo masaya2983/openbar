bf77e23df3b8191f2511dd8c92751c6b9d62b68c
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-12 13:02:02 +0000

    お酒の組み合わせ系のサイト

--- app/controllers/publics/users_controller.rb
+++ app/controllers/publics/users_controller.rb
@@ -1,2 +1,9 @@
 class Publics::UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    @liquors = @user.games.page(params[:page]).per(10)
+    @liquor = Game.new
+  end
+    
 end
