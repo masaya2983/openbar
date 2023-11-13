39f9996200555bcdeda2826791a357d91559ad29
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-13 12:26:06 +0000

    お酒の組み合わせ系のサイト

--- app/controllers/public/favorits_controller.rb
+++ app/controllers/public/favorits_controller.rb
@@ -1,2 +1,3 @@
 class Public::FavoritsController < ApplicationController
+  
 end
--- app/controllers/public/liquors_controller.rb
+++ app/controllers/public/liquors_controller.rb
@@ -1,2 +1,19 @@
 class Public::LiquorsController < ApplicationController
+  def show
+    @liquor = Liquor.find(params[:id])
+    @genre = @liquor.genre
+    @liquorcomment = Comment.new
+    @liquorcomment = Comment.all.page(params[:page]).per(4)
+    if @liquor.status_private? && @liquor.user !=current_user
+   respond_to do |format|
+    format.html { redirect_to liquors_path, notice: 'このページにはアクセスできません' }
+     end
+    end
+  end
+  
+  def index
+   @liqors = Liquor.all
+   
+  end
+  
 end
