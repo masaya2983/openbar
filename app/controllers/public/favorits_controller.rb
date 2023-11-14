baa0ce988b3d4f3a727650c5d9da7bd0b0ae65c4
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-14 14:03:03 +0000

    お酒の組み合わせ系のサイト

--- app/controllers/public/favorits_controller.rb
+++ app/controllers/public/favorits_controller.rb
@@ -1,3 +1,10 @@
 class Public::FavoritsController < ApplicationController
   
+  def create
+    liquor = Liquor.find(params[:id])
+    @favorite = current_user.favorites.new(liquor_id: liquor.id)
+    @favorite.save
+   render 'replace_btn'
+  end
+  
 end
