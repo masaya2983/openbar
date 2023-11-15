d792e290cb83f1f509f48449f9287d3e71ae45df
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-15 13:05:18 +0000

    お酒の組み合わせ系のサイト

--- app/controllers/public/favorits_controller.rb
+++ app/controllers/public/favorits_controller.rb
@@ -7,4 +7,21 @@ class Public::FavoritsController < ApplicationController
    render 'replace_btn'
   end
   
+  def destroy
+     liquor = Liquor.find(params[:liquor_id])
+     @favorite = current_user.favorites.find_by(liquor_id: liquor.id)
+     @favorite.destroy
+      render 'replace_btn'
+  end
+  
+  def followings
+     user = User.find(params[:user_id])
+     @users = user.followings
+  end
+  #フォロワー一覧
+  def followers
+     user = User.find(params[:user_id])
+     @users = user.followers
+  end
+  
 end
