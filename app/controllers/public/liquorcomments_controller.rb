5261cff08626933726fcef2267f0a21a1c0706c4
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-15 14:12:05 +0000

    お酒の組み合わせ系のサイト

--- app/controllers/public/liquorcomments_controller.rb
+++ app/controllers/public/liquorcomments_controller.rb
@@ -1,2 +1,14 @@
 class Public::LiquorcommentsController < ApplicationController
+  
+  def create
+    @liquorcomment = current_user.liquorcomments.new(liquorcomment_params)
+     if @liquorcomment.save
+       flash[:notice] = "success"
+      redirect_to @liquorcomment.liquor
+     else
+      flash.now[:danger] = "failed"
+      render 'public/liquors/show'
+     end
+  end
+  
 end
