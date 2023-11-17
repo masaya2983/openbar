9a166351e048a1f9cb2b0846d5742d793b69d359
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-17 13:21:46 +0000

    お酒の組み合わせ系のサイト

--- app/controllers/admin/liquors_controller.rb
+++ app/controllers/admin/liquors_controller.rb
@@ -1,2 +1,28 @@
 class Admin::LiquorsController < ApplicationController
+    before_action :authenticate_admin!
+  def index
+    @liqors = Liqor.order(created_at: :desc).page(params[:page]).per(6)
+  end
+  
+  def show
+    @liqor = Liqor.find(params[:id])
+  end
+
+  def update
+       @liquor == Liquor.find(params[:id])
+    if @liquor.update(liquor_params)
+      redirect_to liquor_path(@liquor)
+      flash[:notice] ="更新完了"
+    else
+    @liqors = Liquor.all
+    render "edit"
+    end  
+  
+  end
+  
+  def destroy
+    @liquor = Liquor.find(params[:id])
+    @liquor.destroy
+    redirect_to adminliquors_path
+  end
 end
