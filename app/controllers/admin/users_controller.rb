049776c3e87676ae2c5ae286e76e896c4807f973
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-17 13:12:57 +0000

    お酒の組み合わせ系のサイト

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,25 @@
 class Admin::UsersController < ApplicationController
+   before_action :authenticate_admin!
+  
+  def index
+    @users = User.all.page(params[:page]).per(6)
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    user = User.find(params[:id])
+    user.update!(is_deleted: false)
+    flash[:notice] = "success"
+    redirect_to admin_user_path(user)
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.update!(is_deleted: true)
+    flash[:notice] = "success"
+    redirect_to admin_user_path(user)
+  end
 end
