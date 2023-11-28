06554dbfc06b86d07b23d23334358fa580381031
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-28 12:17:44 +0000

    お酒の組み合わせ系のサイト

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,8 +1,26 @@
 class Public::UsersController < ApplicationController
    def show
     @user = User.find(params[:id])
-    @liquors = @user.games.page(params[:page]).per(10)
-    @liquor = Game.new
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    if @user.id == current_user.id
+    else
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+        @liquors = @user.games.page(params[:page]).per(10)
+    @liquor = Liquor.new
+    end
    end
     
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,8 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
+  resources :messages, only: [:create]
+resources :rooms, only: [:create,:show]
 
   get "search" => "searches#search"
   resources :liquors, only: [:index,:create,:show,:update,:destroy,:edit,:new] do
