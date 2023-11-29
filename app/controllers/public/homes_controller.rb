22e79139d74db99eed7136dde6e7af56b0c03ead
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-29 12:32:39 +0000

    お酒の組み合わせ系のサイト

---
+++ app/controllers/public/homes_controller.rb
@@ -0,0 +1,16 @@
+class Public::HomesController < ApplicationController
+  def top
+  end
+
+  def about
+  end
+
+  def gest_sign_in
+   user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+   user.password = SecureRandom.urlsafe_base64
+  end
+   sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+  
+end
