f2f7bc705ea81baf1ba4da036cb60e9ed72b1b5d
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-12-02 12:22:58 +0000

    お酒の組み合わせ系のサイト

--- app/models/appetizer.rb
+++ app/models/appetizer.rb
@@ -1,2 +1,26 @@
 class Appetizer < ApplicationRecord
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
+  scope :star_count, -> {order(star: :desc)}
+
+ validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+  #validates :category,presence:true
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+   def self.search_for(content, method)
+    if method == 'perfect'
+      Game.where(title: content)
+    elsif method == 'forward'
+      Game.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Game.where('title LIKE ?', '%'+content)
+    else
+      Game.where('title LIKE ?', '%'+content+'%')
+    end
+   end
 end
--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,9 +1,6 @@
 class Bar < ApplicationRecord
   belongs_to :user
-  belongs_to :genre
-  has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
   scope :latest, -> {order(created_at: :desc)}
   scope :old, -> {order(created_at: :asc)}
   scope :star_count, -> {order(star: :desc)}
