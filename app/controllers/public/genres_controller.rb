63aa6245778c1af240612f8e64ceeb2918da5ab1
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-12-04 11:01:09 +0000

    お酒の組み合わせ系のサイト

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -1,2 +1,8 @@
 class Public::GenresController < ApplicationController
+  def search
+    @model = params[:model]
+		@content = params[:content]
+    @liquors = Game.where(category_id: params[:category])
+    render "public/searches/result"
+  end
 end
